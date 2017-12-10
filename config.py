2ec388c1747f64b0eb147aa0aa7e9e8c5755ea2b
slm15 <samuel.mnet@gmail.com>
2017-12-10 18:01:09 +0100

    Changes to be committed: 	modified:   .gitignore 	new file:   Procfile 	new file:   config.py 	deleted:    reqPy3.txt 	new file:   requirements.txt

---
+++ config.py
@@ -0,0 +1,12 @@
+# These variables are in version control.
+# This file contains most of the configuration variables that your app needs.
+# it s read before instance/config.py, so it overwritten by it
+DEBUG = False
+
+
+
+
+# # variables that should not be in version control
+# # it s read after ../config.py, so it overwrites it
+# # specific to the local instance
+# DEBUG = True
