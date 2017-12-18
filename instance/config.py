96010350f38d06da8217fdecf9cb6ff59d0170c6
slm15 <samuel.mnet@gmail.com>
2017-12-18 21:19:02 +0100

    removed instance

--- instance/config.py
+++
@@ -1,5 +0,0 @@
-# variables that should not be in version control
-# it s read after ../config.py, so it overwrites it
-# specific to the local instance
-MY_VARIABLE = 777
-DEBUG = False  # THIS FILE IS NOT READ WHAT SO EVER
